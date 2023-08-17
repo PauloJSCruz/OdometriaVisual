ca47d0f9ecef9fb39492ec43e7ebc4e1cda54421
Paulo Cruz <1141513@isep.ipp.pt>
2023-08-17 19:10:18 +0100

    Cleaned code

--- OdometriaVisual_TomasiHarris.py
+++ OdometriaVisual_TomasiHarris.py
@@ -9,8 +9,9 @@ import os
 import random
 import logging
 # endregion
-SaveImage = False
 
+#region configs
+SaveImage = False
 # region save images
 # Flag para guardar as imagens, se false apenas mostra a imagem final
 DataTimeNow = datetime.now()
@@ -31,156 +32,10 @@ logging.basicConfig(filename=f'{OutputFolderDataLogger}/dataLogger_{DataTimeNow.
 # Criando um objeto de log
 dataLogger = logging.getLogger('dataLogger')
 
-# region Not Used
-class LinearModel:
-    def __init__(self):
-        self.slope = 0
-        self.intercept = 0
-
-    def fit(self, data):
-        x = [point[0] for point in data]
-        y = [point[1] for point in data]
-        if len(x) > 0:
-            self.slope, self.intercept = np.polyfit(x, y, 1)
-
-    def distance(self, point):
-        x, y = point
-        expected_y = self.slope * x + self.intercept
-        return abs(y - expected_y)
-
-
-# region Print Ransac
-def RansacPrint(frame, newCorners):
-    # region Definir os parâmetros para o algoritmo RANSAC
-    numOfCoordinatesMin = 10  # Número mínimo de pontos para ajustar o modelo
-    k = 100  # Número de iterações do RANSAC
-    distanceToConsiderInlier = 25  # Limiar para considerar um ponto como inlier
-    minInlinersForAccept = 10  # Número mínimo de inliers para aceitar o modelo
-    # endregion
-
-    # Criar uma instância do modelo linear
-    linearModel = LinearModel()
-
-    # Converter os pontos em um formato adequado para o RANSAC
-    data = [(point[0][0], point[0][1]) for point in newCorners]
-
-    # Executar o algoritmo RANSAC
-    bestModel, bestConsensusSet = Ransac(data, linearModel, numOfCoordinatesMin, k, distanceToConsiderInlier, minInlinersForAccept)
-
-    if bestModel is not None:
-        x = np.array([point[0] for point in bestConsensusSet])
-        y = bestModel.slope * x + bestModel.intercept
-        for i in range(len(x) - 1):
-            cv2.line(frame, (int(x[i]), int(y[i])), (int(
-                x[i+1]), int(y[i+1])), (0, 255, 0), 2)
-        DrawOutliersAndInliers(
-            frame, data, bestModel, distanceToConsiderInlier)
-
-
-def DrawOutliersAndInliers(frame, data, model, distanceToConsiderInlier):
-    # Função para desenhar outliers e inliers no quadro
-
-    for point in data:
-        # Verificar se o ponto é um outlier ou inlier
-        if model.distance(point) > distanceToConsiderInlier:
-            color = (0, 0, 255)  # Vermelho (outlier)
-        else:
-            color = (0, 255, 0)  # Verde (inlier)
-        # Converter as coordenadas do ponto para inteiros
-        center = (int(point[0]), int(point[1]))
-        # Desenhar a bola no quadro
-        cv2.circle(frame, center, 5, color, -1)
-
-
-def DrawTracks(frame, mask, goodNew, goodOld, color):
-    # Desenhar as linhas que representam o fluxo óptico nos quadros
-    for i, (new, old) in enumerate(zip(goodNew, goodOld)):
-        a, b = new.ravel()
-        c, d = old.ravel()
-        a, b, c, d = int(a), int(b), int(c), int(d)  # Convert to integers
-        mask = cv2.line(mask, (a, b), (c, d), (0, 255, 0), 2)
-        frame = cv2.circle(frame, (a, b), 5, (0, 0, 255), 2)
-
-    frameMask = cv2.add(frame, mask)
-    return frameMask
-# endregion
-
-
-def ReadFilesTxt(file):
-    with open(file, 'r') as f:
-        linhas = f.readlines()
-        # Inicialize uma lista vazia para armazenar os valores da matriz
-        valores = []
-        # Percorra cada linha
-        for linha in linhas:
-            # Remova os caracteres de espaço em branco e os colchetes
-            linha = linha.strip().strip('[]')
-
-            # Divida a linha em elementos separados por espaço em branco
-            elementos = linha.split()
-
-            # Converta cada elemento para float e adicione-o à lista de valores
-            valores.extend(map(float, elementos))
-    return valores
-
-
-def CortarMetadeInferior(frame):
-    altura, largura, _ = frame.shape
-    metade_inferior = frame[altura//2:altura, :]
-    return metade_inferior
-
-
-def Ransac(data, model, numOfCoordinatesMin, k, distanceToConsiderInlier, minInlinersForAccept):
-    bestModel = None
-    bestConsensusSet = None
-    maxInliers = 0
-
-    if len(data) < numOfCoordinatesMin:
-        numOfCoordinatesMin = len(data)
-
-    for i in range(k):
-        sample = random.sample(data, numOfCoordinatesMin)
-        maybeInliers = [point for point in data if point not in sample]
-        model.fit(sample)
-        consensusSet = sample.copy()
-
-        for point in maybeInliers:
-            if model.distance(point) < distanceToConsiderInlier:
-                consensusSet.append(point)
-
-        if len(consensusSet) > minInlinersForAccept:
-            if len(consensusSet) > maxInliers:
-                maxInliers = len(consensusSet)
-                bestModel = model
-                bestConsensusSet = consensusSet
-
-    return bestModel, bestConsensusSet
-
-
-def FrameSidebySide(prevFrame, frame, prevDetectedCorners, trackedCorners):
-    # Teste
-    # Definir a largura e altura desejadas do frame
-    FrameWidth = 1680
-    FrameHeight = 700
-    combined_frame = cv2.hconcat((frame, prevFrame))
-    # Desenhar uma linha entre os pontos nos quadros atual e anterior
-    if prevDetectedCorners is not None:
-        for i in range(len(trackedCorners)):
-            prev_pt = tuple(map(int, prevDetectedCorners[i].ravel()))
-            current_pt = tuple(map(int, trackedCorners[i].ravel()))
-            # Gerar uma cor RGB aleatória
-            color = tuple(np.random.randint(0, 255, 3).tolist())
-            # Desenhar a linha com a cor aleatória no quadro combinado
-            cv2.line(combined_frame, prev_pt, (current_pt[0] + prevFrame.shape[1], current_pt[1]), color, 1)
-            # Desenhar círculos sem preenchimento nas extremidades da linha
-            cv2.circle(combined_frame, prev_pt, 5, color, -1)
-            cv2.circle(combined_frame, (current_pt[0] + prevFrame.shape[1], current_pt[1]), 5, color, -1)
-
-            # Rodar e Redimensionar o frame para a largura e altura desejadas
-            combined_frame = cv2.resize(combined_frame, (FrameWidth, FrameHeight))
-            cv2.imshow('Combined Frames', combined_frame)
-    # 
-# endregion
+# Initialize variables for camera motion and position
+prevCameraMotion = np.eye(4)
+prevCameraPosition = np.zeros(3)
+#endregion
 
 
 def CaptureNewKittiFrame():
@@ -201,7 +56,7 @@ def CaptureNewKittiFrame():
     dataLogger.info('\n Kitti video is ready \n')
 
 
-def DisplayFrame(textOnFrame):
+def PrintFrame(textOnFrame):
     # Definir a largura e altura desejadas do frame
     FrameWidth = 1024
     FrameHeight = 700
@@ -405,51 +260,6 @@ def DecomporMatrizEssencial(essentialMatrix, goodOldCorners, trackedCorners, cam
     return breakDownRotation, breakDownTranslation
 
 
-# Print Output
-def UpdateCameraMotion(R, T, prevCameraMotion, prevCameraPosition):
-    # Atualiza a estimativa de movimento da câmera
-    cameraMotion = np.dot(prevCameraMotion, np.hstack((R, T)))
-
-    # Atualiza a posição estimada da câmera no mundo
-    cameraPosition = prevCameraPosition + np.dot(prevCameraMotion[:, :3], T)
-
-    # Reduz o espaçamento horizontal entre os subplots
-    # plt.subplots_adjust(wspace=0.01)
-    # Reduz o espaçamento vertical entre os subplots
-    # plt.subplots_adjust(hspace=0.01)
-
-    plotTrajectory = plotsFinish[0, 0]
-    # Plot da cameraPosition
-    plotTrajectory.set_xlabel('X')
-    plotTrajectory.set_ylabel('Y')
-    plotTrajectory.axis('equal')
-    plotTrajectory.set_title('Plot Camera Position')
-    plotTrajectory.plot(cameraPosition[0], cameraPosition[1], 'ro')
-
-    # Plot da cameraMotion
-    # region plot 3D
-    # # Adiciona o subplot 3D em plots[1, 2]
-    # plotMotion3D = plotsFinish[1, 1]
-    # plotMotion3D = fig.add_subplot(2, 2, 4, projection='3d')
-    # plotMotion3D.quiver(
-    #     0, 0, 0, cameraMotion[0, 3], cameraMotion[1, 3], cameraMotion[2, 3])
-    # plotMotion3D.set_xlim([-1, 1])
-    # plotMotion3D.set_ylim([-1, 1])
-    # plotMotion3D.set_zlim([-1, 1])
-
-    # plotMotion3D.set_xlabel('X')
-    # plotMotion3D.set_ylabel('Y')
-    # plotMotion3D.set_zlabel('Z')
-    # plotMotion3D.set_title('Plot Camera Motion')
-    # endregion
-
-    plt.tight_layout()
-    plt.show()
-    plt.pause(0.1)
-
-    return cameraMotion, cameraPosition
-
-
 def PlotFrames(frames):
     # region plots parameters
     figFrames, axsFrames = plt.subplots(1, 3, figsize=(24, 12), dpi=300)
@@ -492,13 +302,6 @@ def LoadCalibrationCamara():
     dataLogger.info(f'\n intrinsicParameters \n {intrinsicParameters}')
     # dataLogger.info(f'\n distortioncoefficient \n {distortioncoefficient}')
     return intrinsicParameters
-
-
-##############################################################################################################################################
-
-# Initialize variables for camera motion and position
-prevCameraMotion = np.eye(4)
-prevCameraPosition = np.zeros(3)
 
 # Function to update camera motion and position
 def updateCameraMotionPosition(relativeRotation, relativeTranslation):
@@ -566,8 +369,6 @@ def DrawTrajectory(trajectory, cameraPosition):
     cv2.imshow('Frame with Trajectory', image)
 
     return image
-
-##############################################################################################################################################
 
 def main():
     # DataTimeNow, OutputFolder, plotsFinish, dataLogger = Init()
@@ -661,7 +462,7 @@ def main():
             textOnFrame.append([frameMask, "FrameId", len(trackedCorners)])
             textOnFrame.append([frameMask, "trackedCorners", len(trackedCorners)])
             textOnFrame.append([frameMask, f"CountFrames {ResetCorners}", CountFrames])
-            DisplayFrame(textOnFrame)
+            PrintFrame(textOnFrame)
             # Reset Text on Frame
             textOnFrame = []
 
